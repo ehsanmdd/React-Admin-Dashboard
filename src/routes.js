bdb78b2c40d769922a52acfc122bf54443766033
Ehsan Motamedi <ehsan.mtd@yahoo.com>
2022-12-31 23:19:56 +0330

    Prepering Routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import Home from "./Pages/Home/Home"
+import UserList from "./Pages/Users/UserList"
+import NewUser from "./Pages/NewUser/NewUser"
+import Products from "./Pages/Products/Products"
 
 let routes = [
-    {path: "/" , element: <Home />}
+    {path: "/" , element: <Home />},
+    {path: "/users" , element: <UserList />},
+    {path: "/newuser" , element: <NewUser />},
+    {path: "/products" , element: <Products />},
 ]
 
 export default routes
