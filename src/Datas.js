4710948c9172e21022a9795352ffdbbd0d312200
Ehsan Motamedi <ehsan.mtd@yahoo.com>
2023-01-03 18:02:46 +0330

    End UsersList Section Development

--- src/Datas.js
+++ src/Datas.js
@@ -86,6 +86,14 @@ const transaction = [
         img : "./images/Avatar/UserImage-7.jpg"
     },
     {
+        id : 5,
+        customer : "Ardeshir Keyhani",
+        date : "25 Nov 2022",
+        amount : "100.85",
+        status : "Pending",
+        img : "./images/Avatar/UserImage-11.jpg"
+    },
+    {
         id : 2,
         customer : "Ghazal Nemati",
         date : "22 Jan 2022",
@@ -101,6 +109,63 @@ const transaction = [
         status : "Declined",
         img : "./images/Avatar/UserImage-9.jpg"
     },
+    {
+        id : 4,
+        customer : "Pedram Sobhani",
+        date : "08 Sep 2022",
+        amount : "280.85",
+        status : "Approved",
+        img : "./images/Avatar/UserImage-10.jpg"
+    },
 ]
 
-export {xAxisData , newMembers , transaction}
+let userTable = [
+
+    {
+        id : 1,
+        userName : "Sasan Sanei",
+        avatar : "./images/Avatar/UserImage-11.jpg",
+        status : "active",
+        age : 25,
+        transaction : "$129.00",
+        email : "s.sanei@gmail.com"
+    },
+    {
+        id : 2,
+        userName : "Sara Zandi",
+        avatar : "./images/Avatar/UserImage-12.jpg",
+        status : "active",
+        age : 27,
+        transaction : "$130.00",
+        email : "mohamadi.emad@yahoo.com"
+    },
+    {
+        id : 3,
+        userName : "Negin Pashaadeh",
+        avatar : "./images/Avatar/UserImage-13.jpg",
+        status : "disable",
+        age : 22,
+        transaction : "$111.00",
+        email : "pashazadeh.n@gmail.com"
+    },
+    {
+        id : 4,
+        userName : "Pegah Ghadimi",
+        avatar : "./images/Avatar/UserImage-14.jpg",
+        status : "active",
+        age : 24,
+        transaction : "$120.00",
+        email : "nr.motlagh@hotmail.com"
+    },
+    {
+        id : 5,
+        userName : "Donya Meshkat",
+        avatar : "./images/Avatar/UserImage-16.jpg",
+        status : "active",
+        age : 24,
+        transaction : "$580.00",
+        email : "d.m@meshkat.com"
+    },
+]
+
+export {xAxisData , newMembers , transaction , userTable}
--- src/Pages/Users/UserList.js
+++ src/Pages/Users/UserList.js
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import { Link } from 'react-router-dom';
+import { userTable } from '../../Datas';
+import "./UserList.css";
+
 
 function UserList() {
+  const [usersData, setUsersData] = useState(userTable);
+  const userDelete = (userID) => { 
+
+    setUsersData(usersData.filter(user => user.id !== userID) )
+  }
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      width: 90,
+    },
+    {
+      field: 'user',
+      headerName: 'User',
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to="/" className='link'>
+              <div className="usersList__User">
+                <img src={params.row.avatar} className="userList__Img" alt='User Avatar' />
+                {params.row.userName}
+              </div>
+            </Link>
+          </>
+        )
+      }
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 200
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction",
+      width: 120
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/${params.row.id}`} className="link">
+              <button className='userList__Edit'>Edit</button>
+            </Link>
+            <DeleteForeverOutlinedIcon
+            className='userList__Delete'
+            onClick={() => userDelete(params.row.id)}
+            />
+          </>
+        )
+      }
+    },
+  ]
+
+
   return (
-    <div>Users</div>
+    <div className='userList'>
+      <span className="userList__title">Users List</span>
+      <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={usersData}
+          columns={columns}
+          pageSize={4}
+          disableSelectionOnClick
+        />
+      </Box>
+    </div >
   )
 }
 
